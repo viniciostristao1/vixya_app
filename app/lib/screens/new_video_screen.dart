032ae0982bdcc95a6b58f94af7c3aa0427c3090f
Vinicius Tristao <viniciostristao@gmail.com>
2026-08-29 19:17:05 +0000

    app: bump v0.1.7+8

--- app/lib/screens/new_video_screen.dart
+++ app/lib/screens/new_video_screen.dart
@@ -234,7 +234,7 @@ class _NewVideoScreenState extends State<NewVideoScreen> {
             label: _sending ? tr('sending') : tr('generate'),
           ),
           const SizedBox(height: 12),
-          Text('v0.1.3 • ${Config.backendUrl}', style: const TextStyle(fontSize: 10, color: Colors.grey), textAlign: TextAlign.center),
+          Text('v0.1.7 • ${Config.backendUrl}', style: const TextStyle(fontSize: 10, color: Colors.grey), textAlign: TextAlign.center),
         ],
       ),
     );
